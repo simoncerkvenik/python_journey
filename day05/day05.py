93ba27a07d9a16d5236f39359d1f37e9742d66a5
Simon <4sahmat@gmail.com>
2026-05-03 17:39:20 +0200

    Add day05 password generator

---
+++ day05/day05.py
@@ -0,0 +1,86 @@
+# PASSWORD GENERATOR - LOGIC ONLY
+
+# 1. Import the module that lets you choose random items
+
+# 2. Create a list of letters
+# include lowercase and uppercase letters
+
+# 3. Create a list of numbers
+# keep them as strings, not integers
+# example: "0", "1", "2"
+
+# 4. Create a list of symbols
+# example: "!", "#", "$", "%", "&"
+
+# 5. Print a welcome message
+# tell the user this is a password generator
+
+# 6. Ask the user:
+# How many letters would you like in your password?
+
+# 7. Ask the user:
+# How many symbols would you like?
+
+# 8. Ask the user:
+# How many numbers would you like?
+
+# 9. Create an empty list for the password
+# this list will store all chosen characters
+
+# 10. Create a loop for letters
+# the loop should repeat as many times as the user asked for letters
+
+# 11. Inside the loop, choose one random letter from the letters list
+
+# 12. Add that random letter to the password list
+
+# 13. Create a loop for symbols
+# the loop should repeat as many times as the user asked for symbols
+
+# 14. Inside the loop, choose one random symbol from the symbols list
+
+# 15. Add that random symbol to the password list
+
+# 16. Create a loop for numbers
+# the loop should repeat as many times as the user asked for numbers
+
+# 17. Inside the loop, choose one random number from the numbers list
+
+# 18. Add that random number to the password list
+
+# 19. Now the password list has all characters
+# but the order is too obvious:
+# letters first, then symbols, then numbers
+
+# 20. Shuffle the password list
+# so the order becomes random
+
+# 21. Create an empty string for the final password
+
+# 22. Loop through every character in the shuffled password list
+
+# 23. Add each character to the final password string
+
+# 24. Print the final password
+print(r"""
+__        __   _                          _ 
+ \ \      / /__| | ___ ___  _ __ ___   ___| |
+  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ |
+   \ V  V /  __/ | (_| (_) | | | | | |  __/_|
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___(_)
+
+        PASSWORD GENERATOR
+
+""")
+import random
+
+list_of_lower_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+list_of_upper_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", "?", "@"]
+
+print(input("How many letters you wont in password? "))
+print(input("How many numbers you wont in password? "))
+print(input("How many symbols you wont in password? "))
